b67efbcd10e92fdd3a6bf414e0c4fa74ebe12b7e
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-06-06 00:07:18 +0300

    Brawse DTO was created

---
+++ resources/assets/js/entities/DTO/Browser.js
@@ -0,0 +1,14 @@
+/**
+ * Browser Entity
+ *
+ * @param clientWidth
+ * @param clientHeight
+ * @constructor
+ */
+
+function Browser (clientWidth = null, clientHeight = null) {
+    this.clientWidth = clientWidth;
+    this.clientHeight= clientHeight;
+}
+
+export { Browser }
