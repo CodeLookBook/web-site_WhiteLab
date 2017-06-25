f6c8ded9d2b07610bd7ba1cb4ec4ac645e8da3a9
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-06-25 17:34:11 +0300

    MainContentSlot class was created.

--- resources/assets/js/components/app/layout/MainContentSlot.ts
+++ resources/assets/js/components/app/layout/MainContentSlot.ts
@@ -1,11 +1,61 @@
-class MainContentSlot{
+/**---------------------------------------------------------------------------
+ * MainContentSlot class
+ * ---------------------------------------------------------------------------
+ * - Represents .mainContentSlot HTML DOM element;
+ * - Gives simple API that helps to get element description information;
+ * - Implements Singleton pattern structure;
+ */
+class MainContentSlot extends DomElement{
 
-    private _domElement: object;
-    private _height: number;
+    // ------------------------------------------------------------------------
+    // Properties.
+    // ------------------------------------------------------------------------
 
-    constructor(height: number){
-        this._height = height;
+    /**
+     * Singleton pattern instance.
+     */
+    protected static _instance: MainContentSlot | null = null;
+
+    // ------------------------------------------------------------------------
+    // Constructor.
+    // ------------------------------------------------------------------------
+
+    constructor(el: HTMLElement){
+        super(el);
     }
 
+    // ------------------------------------------------------------------------
+    // Methods.
+    // ------------------------------------------------------------------------
 
+    /**
+     * Creates MainContentSlot instance.
+     * @param el - HTML DOM element.
+     * @return {MainContentSlot}
+     */
+    private static create(el: HTMLElement): MainContentSlot {
+        return new MainContentSlot(el);
+    }
+
+    /**
+     * Returns MainContentSlot instance.
+     * @param el  - HTML DOM element '.MAIN-CONTENT-SLOT'
+     * @return    - Singleton pattern instance.
+     */
+    static getInstance(el: HTMLElement): MainContentSlot{
+
+        //Create instance.
+        if(MainContentSlot._instance === null){
+            MainContentSlot._instance = MainContentSlot.create(el);
+        }
+
+        //If '_instance' still equal to 'null' throw exception.
+        if(MainContentSlot._instance === null){
+            throw new ReferenceError();
+        }
+
+        return MainContentSlot._instance;
+    }
 }
+
+
