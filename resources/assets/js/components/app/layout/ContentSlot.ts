514944d46a3fc785138939767c0a27e75c77ee79
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-06-25 17:30:36 +0300

    ContentSlot class was created.

--- resources/assets/js/components/app/layout/ContentSlot.ts
+++ resources/assets/js/components/app/layout/ContentSlot.ts
@@ -1,3 +1,59 @@
-class ContentSlot{
+/**---------------------------------------------------------------------------
+ * ContentSlot class
+ * ---------------------------------------------------------------------------
+ * - Represents '.CONTENT-SLOT' HTML DOM element;
+ * - Gives simple API that helps to get element description information;
+ * - Implements Singleton pattern structure;
+ */
+class ContentSlot extends DomElement{
 
+    // ------------------------------------------------------------------------
+    // Properties.
+    // ------------------------------------------------------------------------
+
+    /**
+     * Singleton pattern instance.
+     */
+    protected static _instance: ContentSlot | null = null;
+
+    // ------------------------------------------------------------------------
+    // Constructor.
+    // ------------------------------------------------------------------------
+
+    constructor(el: HTMLElement){
+        super(el);
+    }
+
+    // ------------------------------------------------------------------------
+    // Methods.
+    // ------------------------------------------------------------------------
+
+    /**
+     * Creates ContentSlot instance.
+     * @param el - HTML DOM element.
+     * @return {ContentSlot}
+     */
+    private static create(el: HTMLElement): ContentSlot {
+        return new ContentSlot(el);
+    }
+
+    /**
+     * Returns ContentSlot instance.
+     * @param el  - HTML DOM element '.CONTENT-SLOT'
+     * @return    - Singleton pattern instance.
+     */
+    static getInstance(el: HTMLElement): ContentSlot{
+
+        //Create instance.
+        if(ContentSlot._instance === null){
+            ContentSlot._instance = ContentSlot.create(el);
+        }
+
+        //If '_instance' still equal to 'null' throw exception.
+        if(ContentSlot._instance === null){
+            throw new ReferenceError();
+        }
+
+        return ContentSlot._instance;
+    }
 }
